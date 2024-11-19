c9109d13c424de5ae03c7ab4cac554d4d61dc290
reubenmiller <reuben.d.miller@gmail.com>
2024-11-19 10:03:35 +0100

    feat: add mosquitto listener for container network

--- recipes/docker/steps/00-install.sh
+++ recipes/docker/steps/00-install.sh
@@ -27,3 +27,6 @@ usermod -aG docker tedge
 
 # Copy Docker persist file
 install -D -m 644 "${RECIPE_DIR}/files/docker.toml" -t /etc/rugpi/state
+
+# Add mosquitto listener which allows other containers access to the thin-edge.io MQTT broker
+install -D -m 0644 "${RECIPE_DIR}/files/tedge-networkcontainer.conf" -t /etc/tedge/mosquitto-conf/
