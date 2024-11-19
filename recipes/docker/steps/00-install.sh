7bdd900d80af6ac15918afa458bbf068b2553288
reubenmiller <reuben.d.miller@gmail.com>
2024-11-19 10:09:18 +0100

    add sudoers rule for container commands

--- recipes/docker/steps/00-install.sh
+++ recipes/docker/steps/00-install.sh
@@ -30,3 +30,6 @@ install -D -m 644 "${RECIPE_DIR}/files/docker.toml" -t /etc/rugpi/state
 
 # Add mosquitto listener which allows other containers access to the thin-edge.io MQTT broker
 install -D -m 0644 "${RECIPE_DIR}/files/tedge-networkcontainer.conf" -t /etc/tedge/mosquitto-conf/
+
+# Add sudoers rules
+install -D -m 0644 "${RECIPE_DIR}/files/suoders.tedge-container-plugin" -T /etc/sudoers.d/tedge-container-plugin
