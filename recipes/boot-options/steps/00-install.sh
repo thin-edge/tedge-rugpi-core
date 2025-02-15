edbf8913c26d8938e4641210e1b443e481fb8130
reubenmiller <reuben.d.miller@gmail.com>
2025-02-15 13:56:01 +0100

    support running under a vm

--- recipes/boot-options/steps/00-install.sh
+++ recipes/boot-options/steps/00-install.sh
@@ -2,12 +2,18 @@
 set -eu
 
 CMDLINE_FILE="${RUGIX_LAYER_DIR}/roots/boot/cmdline.txt"
+mkdir -p "${RUGIX_LAYER_DIR}/roots/boot"
 
 echo "Modifying boot cmdline.txt to enable cgroup memory monitoring" >&2
-echo "File: $CMDLINE_FILE (before)" >&2
-cat "$CMDLINE_FILE" >&2
 
-CMDLINE_CONTENTS=$(cat "$CMDLINE_FILE")
+CMDLINE_CONTENTS=""
+if [ -f "$CMDLINE_FILE" ]; then
+    cat "$CMDLINE_FILE" >&2
+    CMDLINE_CONTENTS=$(cat "$CMDLINE_FILE")
+fi
+
+echo "File: $CMDLINE_FILE (before)" >&2
+echo "$CMDLINE_CONTENTS"
 
 # remove duplicates, use xargs to trim leading and trailing spaces
 CMDLINE_CONTENTS=$(echo "$CMDLINE_CONTENTS" | sed 's/cgroup_memory=[a-z0-9A-Z,]*//g' | xargs)
