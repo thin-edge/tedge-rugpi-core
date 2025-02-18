24778af9c2520675f39a0a1ed6d48b5f898e69de
reubenmiller <reuben.d.miller@gmail.com>
2025-02-18 22:28:09 +0100

    disable the docker service by default for debugging

--- recipes/docker/steps/00-install.sh
+++ recipes/docker/steps/00-install.sh
@@ -33,3 +33,6 @@ install -D -m 0644 "${RECIPE_DIR}/files/tedge-networkcontainer.conf" -t /etc/ted
 
 # Add sudoers rules
 install -D -m 0644 "${RECIPE_DIR}/files/suoders.tedge-container-plugin" -T /etc/sudoers.d/tedge-container-plugin
+
+# FIXME: Enable the service by default by disable it due to an issue when running in a vm
+systemctl disable docker.service
