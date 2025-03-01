92179fde3adb7c47fa6492600d75dfd539bccaf4
reubenmiller <reuben.d.miller@gmail.com>
2025-03-01 22:26:17 +0100

    cleanup parsec recipe

---
+++ recipes/parsec/steps/01-install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+apt-get update
+apt-get install -y \
+    parsec-service \
+    parsec-tool \
+    softhsm2 \
+    opensc
+
+install -D -m 644 "${RECIPE_DIR}/files/parsec-init.sh" -t /usr/bin/
+
+# provider config
+install -D -m 644 "${RECIPE_DIR}/files/tpm-provider.toml" -t /usr/share/parsec/
+
+# Note: The pkcs11-provider is created by the parsec-init.sh script as the configure requires
+# some machine specific config (e.g. the path to the .so file which usually is different per CPU architecture)
+
+# Use symlink so that the workflow file can be updated within the image
+ln -s /usr/share/parsec/tpm-provider.toml /etc/parsec/config.tpm-provider.toml
