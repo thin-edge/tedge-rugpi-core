0617db8e523f54d52d4cdc7bae1a58ab7fd3a406
reubenmiller <reuben.d.miller@gmail.com>
2025-03-01 23:22:43 +0100

    add wip recipe for k3s

---
+++ recipes/k3s/steps/01-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+# Install k3s in the server role and use secrets encryption by default
+curl -sfL https://get.k3s.io | sh -s - server --secrets-encryption
+
+# Copy podman persist file
+install -D -m 0644 "${RECIPE_DIR}/files/k3s.toml" -t /etc/rugix/state
+
+# Add sudoers rules
+install -D -m 0644 "${RECIPE_DIR}/files/suoders.k3s" -T /etc/sudoers.d/k3s
+
+# Install helm cli (the helm controller is included in k3s by default)
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm -f ./get_helm.sh
+
+# Set default (warning, this is only for the root user)
+# Add default local but there is not guarantee that any user will reference it
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /etc/environment
+echo "[ -f /etc/environment ] && . /etc/environment" >> "$HOME/.zshrc"
