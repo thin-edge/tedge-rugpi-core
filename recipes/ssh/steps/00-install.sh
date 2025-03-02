421bec107da11bb5c0c3b141e2ef2cfe949086df
reubenmiller <reuben.d.miller@gmail.com>
2025-03-02 15:09:52 +0100

    feat: support loading public ssh keys from github user accounts

--- recipes/ssh/steps/00-install.sh
+++ recipes/ssh/steps/00-install.sh
@@ -41,3 +41,39 @@ if [ -n "${RECIPE_PARAM_ROOT_AUTHORIZED_KEYS}" ] || [ -n "$SSH_KEYS" ]; then
     chmod -R 600 /root/.ssh
     cat /root/.ssh/authorized_keys >&2
 fi
+
+#
+# Retrieve public ssh keys from GitHub
+#
+download_keys_from_github() {
+    for gh_username in $(echo "$1" | tr ',' '\n' | xargs -n1); do
+        if [ -n "$gh_username" ]; then
+            gh_url="https://github.com/${gh_username}.keys"
+            public_ssh_key=$(curl -fsSL "$gh_url" ||:)
+            if [ -n "$public_ssh_key" ]; then
+                echo "Adding key/s from Github User ${gh_username}. url=${gh_url}" >&2
+                public_ssh_key=$(printf '\n# %s\n%s\n' "$gh_username" "$public_ssh_key")
+                echo "  $public_ssh_key" >&2
+                add_ssh_key "$public_ssh_key"
+            else
+                echo "WARNING: Could not find a public ssh key. url=$gh_url" >&2
+            fi
+        fi
+    done
+}
+
+# From recipe parameters
+if [ -n "${RECIPE_PARAM_ROOT_AUTHORIZED_KEYS_GITHUB_USERS:-}" ]; then
+    download_keys_from_github "$RECIPE_PARAM_ROOT_AUTHORIZED_KEYS_GITHUB_USERS"
+fi
+
+# From .env file
+if [ -n "${SSH_GITHUB_USERS:-}" ]; then
+    download_keys_from_github "$SSH_GITHUB_USERS"
+fi
+
+# Additional Github user list to make it easier the forked repo to add the owner without
+# having to splice in with the SSH_GITHUB_USERS list
+if [ -n "${SSH_GITHUB_OWNER:-}" ]; then
+    download_keys_from_github "$SSH_GITHUB_OWNER"
+fi
