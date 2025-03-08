2bed7c3f75bd2e604c21d34e3092ee576a430f2d
reubenmiller <reuben.d.miller@gmail.com>
2025-03-08 16:18:11 +0100

    feat(tedge-raspios): store helper scripts under more generic name tedge-hsm

--- recipes/raspi-tpm/steps/01-install.sh
+++ recipes/raspi-tpm/steps/01-install.sh
@@ -10,5 +10,5 @@ dtoverlay=tpm-slb9673
 EOT
 
 # Add helper scripts
-install -m 0755 -d /usr/share/tedge-tpm/bin
-install -D -m 0755 "${RECIPE_DIR}/files/init-tpm.sh" -t /usr/share/tedge-tpm/bin/
+install -m 0755 -d /usr/share/tedge-hsm/bin
+install -D -m 0755 "${RECIPE_DIR}/files/init-tpm.sh" -t /usr/share/tedge-hsm/bin/
