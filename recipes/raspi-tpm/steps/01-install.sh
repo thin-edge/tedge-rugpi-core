aaa19f9dbed8bd7c601cccbefde93aceec2d4cb6
reubenmiller <reuben.d.miller@gmail.com>
2025-03-08 20:47:17 +0100

    fix(tpm): add tedge user to tss group

--- recipes/raspi-tpm/steps/01-install.sh
+++ recipes/raspi-tpm/steps/01-install.sh
@@ -9,6 +9,9 @@ dtoverlay=tpm-slb9670
 dtoverlay=tpm-slb9673
 EOT
 
+# Allow tedge user to access the tpm
+usermod -a -G tss tedge
+
 # Add helper scripts
 install -m 0755 -d /usr/share/tedge-hsm/bin
 install -D -m 0755 "${RECIPE_DIR}/files/init-tpm.sh" -t /usr/share/tedge-hsm/bin/
