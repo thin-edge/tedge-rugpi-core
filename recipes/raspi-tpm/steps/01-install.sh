aea9061156bea3b1d003ef32edf1a3a723e487fb
reubenmiller <reuben.d.miller@gmail.com>
2025-04-30 16:28:08 +0200

    fix(tpm): disable tcsd service by default

--- recipes/raspi-tpm/steps/01-install.sh
+++ recipes/raspi-tpm/steps/01-install.sh
@@ -18,3 +18,8 @@ usermod -u "$RECIPE_PARAM_TSS_USER_ID" tss
 
 # Add helper scripts
 install -m 0755 -d /usr/share/tedge-hsm/bin
+
+# NOTE: disable the service as if the user does not have a TPM installed
+# it results in systemd being marked as degraded due to this service failing
+# Device with a TPM can just enable this service if they need it
+systemctl disable tcsd.service ||:
