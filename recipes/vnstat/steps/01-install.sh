26633e672243e0c2556e1dcbac6cf1454a9d2bcf
reubenmiller <reuben.d.miller@gmail.com>
2025-09-17 09:02:23 +0200

    fix(vnstat): resolved inflated network statistics caused by A/B partition swap

--- recipes/vnstat/steps/01-install.sh
+++ recipes/vnstat/steps/01-install.sh
@@ -6,3 +6,7 @@ install -D -m 644 "${RECIPE_DIR}/files/vnstat-database.toml" -t /etc/rugix/state
 
 groupmod -g 116 vnstat
 usermod -u 116 vnstat
+
+# modify vnstatd service to use --sync which prevents the interface statistics
+# being inflated when switching A/B partitions during a firmware update
+install -D -m 644 "${RECIPE_DIR}/files/override.conf" -t /etc/systemd/system/vnstat.service.d/
