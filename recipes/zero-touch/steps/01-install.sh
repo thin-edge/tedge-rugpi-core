d64a5563cc5488bd970fb315ac1a7c76d955e0d7
reubenmiller <reuben.d.miller@gmail.com>
2026-04-27 09:41:40 +0200

    fixup! feat: add zero-touch provisioning recipe

--- recipes/zero-touch/steps/01-install.sh
+++ recipes/zero-touch/steps/01-install.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 set -eu
 install -D -m 644 "${RECIPE_DIR}/files/ztp-agent.toml" -t /etc/rugix/state/
+
+# Unblock bluetooth as it is needed for discovery
+if [ "$RECIPE_PARAM_DISABLE_RFKILL" = "true" ] && [ -d /var/lib/systemd/rfkill ]; then
+    echo "Enabling bluetooth on devices by default" >&2
+    for filename in /var/lib/systemd/rfkill/*:bluetooth; do
+        echo 0 > "$filename"
+    done
+fi
